3efc3ebd8269fc443b16770212e324fddb46b6ff
daotiensyyyy <sydao1579@gmail.com>
2021-10-28 12:16:45 +0700

    heroku

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,8 @@ const Role = db.role;
 
 async function connect() {
     try {
-        // await mongoose.connect('mongodb+srv://dbUser:123456zxc@cluster0.jwawn.mongodb.net/nodejs-restful-api?retryWrites=true&w=majority', {
-        await mongoose.connect('mongodb://localhost:27017/tea', {
+        await mongoose.connect('mongodb+srv://sydao1579:123456zxc@cluster0.ryy23.mongodb.net/tea?retryWrites=true&w=majority', {
+            // await mongoose.connect('mongodb://localhost:27017/tea', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
